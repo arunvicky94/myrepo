469308c9a31ad29cbcf0612a8cc3f19ef6643122
arun <arunkumar.s@ideas2it.com>
2017-04-27 12:23:38 +0530

    Task: added comment line

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -4,6 +4,7 @@ angular
       var vm = this;
       vm.user = {};
       vm.users = [];
+      //Demo comment
       vm.addUser = function() {
         console.log("controlle calling" + vm.user);
         bookingService.saveUser(vm.user);
